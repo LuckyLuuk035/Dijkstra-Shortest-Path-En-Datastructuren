0c6e2d0e054535a6ff41e8e8dc79d0207824c062
LuckyLuuk035 <LuckyLuuk035@users.noreply.github.com>
2021-05-27 14:48:10 +0200

    Update Stap.java

--- src/Stap.java
+++ src/Stap.java
@@ -1,53 +1,47 @@
 public class Stap {
-    protected int stap;
-    public int afstand;
+    int naar;
+    double afstand;
 
-    public Stap() {
-
-    }
-
-    public Stap(int stap, int afstand) {
-        this.stap = stap;
+    public Stap(int naar, double afstand) {
+        this.naar = naar;
         this.afstand = afstand;
     }
-
-
-
-    class Vlucht {
-        private double prijs;
-        private double kans = 1.00;
-        private double kosten;
-
-        public double getPrijs() {
-            return prijs;
-        }
-
-        public void setPrijs() {
-            this.prijs = afstand * 15;
-        }
-
-        public double getKans() {
-            return kans;
-        }
-
-        public void setKans() {
-            this.kans = kans * 1.03;
-        }
-
-        public void setKosten() {
-            this.kosten = this.prijs * this.kans;
-        }
-    }
-
-    class rit {
-        private double km;
-
-        public void setKm() {
-            this.km = afstand * 10;
-        }
-    }
-
-    class treinrit {
-        private double tijd;
-    }
 }
+
+//     class Vlucht {
+//        private double prijs;
+//        private double kans = 1.00;
+//        private double kosten;
+//
+//        public double getPrijs() {
+//            return prijs;
+//        }
+//
+//        public void setPrijs() {
+//            this.prijs = afstand * 15;
+//        }
+//
+//        public double getKans() {
+//            return kans;
+//        }
+//
+//        public void setKans() {
+//            this.kans = kans * 1.03;
+//        }
+//
+//        public void setKosten() {
+//            this.kosten = this.prijs * this.kans;
+//        }
+//    }
+//
+//    class rit {
+//        private double km;
+//
+//        public void setKm() {
+//            this.km = afstand * 10;
+//        }
+//    }
+//
+//    class treinrit {
+//        private double tijd;
+//    }
